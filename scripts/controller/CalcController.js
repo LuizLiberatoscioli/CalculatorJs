053297fffa18aab7d9ff2e939e6623b361a4ee08
LuizPaulo <luizhat10@gmail.com>
2023-03-02 11:45:43 -0300

    arrumando botoes

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor(){
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -14,11 +15,11 @@ class CalcController {
 
     initialize(){
 
-        this.setDisplayDateTime();
+        this.setDisplayDateTime()
 
-       let interval =  setInterval ( ()=> {
+        setInterval(()=>{
 
-        this.setDisplayDateTime();
+            this.setDisplayDateTime();
 
         }, 1000);
 
@@ -34,6 +35,85 @@ class CalcController {
     
     }
 
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+        
+    }
+
+    execBtn(value){
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+                
+                break;
+
+            case 'subtracao':
+                
+                break;
+
+            case 'divisao':
+                
+                break;
+
+            case 'porcento':
+                
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
+        }
+
+    }
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
@@ -42,7 +122,9 @@ class CalcController {
 
             this.addEventListenerAll(btn, "click drag", e => {
 
-                console.log(btn.className.baseVal.replace("btn-",""));
+                let textBtn = btn.className.baseVal.replace("btn-","");
+
+                this.execBtn(textBtn);
 
             })
 
@@ -57,29 +139,38 @@ class CalcController {
     }
 
     setDisplayDateTime(){
+
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
             month: "long",
             year: "numeric"
-
         });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
     }
 
     get displayTime(){
+
         return this._timeEl.innerHTML;
+
     }
 
-    set displayTime (value){
-        this._timeEl.innerHTML = value;
+    set displayTime(value){
+
+        return this._timeEl.innerHTML = value;
+
     }
 
     get displayDate(){
+
         return this._dateEl.innerHTML;
+
     }
 
-    set displayDate (value){
+    set displayDate(value){
+
         return this._dateEl.innerHTML = value;
+
     }
 
     get displayCalc(){
